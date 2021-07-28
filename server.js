b504d1d8f271f9fc5d15394e4070964d23e3380e
lokeshKumar <lokeshkumargithub@gmail.com>
2021-07-28 22:34:14 +0530

    socketio

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.static(path.resolve(__dirname + '/public')))
 app.get('/', (req, res) =>{
     res.render('index')
 })
-
+ 
+const io = require('socket.io')
 
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
