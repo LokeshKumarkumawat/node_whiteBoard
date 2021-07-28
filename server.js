b5bc7486f7415078fbca6c15ae6b5922028f1665
lokeshKumar <lokeshkumargithub@gmail.com>
2021-07-28 20:41:12 +0530

    Initial

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express =  require('express')
+const app = express()
+const http = require('http').createServer(app)
+const path = require('path')
+
+
+app.use(express.static(path.resolve(__dirname + '/public')))
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+
+
+app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
